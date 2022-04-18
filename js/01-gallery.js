eec76bf95fc87bc20ecd8550ac7d076cad2f3d95
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-04-18 11:25:59 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imgContainer = document.querySelector('.gallery');
+const cardsMarkup = createImgCards(galleryItems);
+imgContainer.insertAdjacentHTML('afterend', cardsMarkup);
+
+function createImgCards(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+  <div class="gallery__item">
+  <a class="gallery__link" href="${description}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `
+    }).join('');
+}
+ 
