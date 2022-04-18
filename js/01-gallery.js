62b54952e42fe43caacb4fc76d0d90cafabca46a
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-04-18 16:38:41 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,13 +5,15 @@ console.log(galleryItems);
 
 const imgContainer = document.querySelector('.gallery');
 const cardsMarkup = createImgCards(galleryItems);
+const imgItem = document.querySelector('.gallery__image')
 imgContainer.insertAdjacentHTML('afterend', cardsMarkup);
+
 
 function createImgCards(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
         return `
   <div class="gallery__item">
-  <a class="gallery__link" href="${description}">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
@@ -23,4 +25,39 @@ function createImgCards(galleryItems) {
     `
     }).join('');
 }
+
+imgContainer.addEventListener('click', onContainerClick);
+
+function onContainerClick(event) {
+   
+    console.log(event.target.classList.contains('gallery__image'));
+  
+  
+
+  
+}
  
+
+// import * as basicLightbox from 'basiclightbox'
+
+// const instance = basicLightbox.create(`
+//     <img src="assets/images/image.png" width="800" height="600">
+// `)
+
+// instance.show()
+
+// import * as basicLightbox from 'basiclightbox'
+
+// const instance = basicLightbox.create(`
+//     <div class="modal">
+//         <p>A custom modal that has been styled independently. It's not part of basicLightbox, but perfectly shows its flexibility.</p>
+//         <input placeholder="Type something">
+//         <a>Close</a>
+//     </div>
+// `, {
+//     onShow: (instance) => {
+//         instance.element().querySelector('a').onclick = instance.close
+//     }
+// })
+
+// instance.show()
