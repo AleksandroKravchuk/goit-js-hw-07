edff83c398f4ee703a675ef076f886c9d373de34
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-04-19 11:29:08 +0300

    2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,13 +1,12 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
 
 const imgContainer = document.querySelector('.gallery');
-const cardsMarkup = createImgCards(galleryItems);
-const imgItem = document.querySelector('.gallery__image')
-imgContainer.insertAdjacentHTML('afterend', cardsMarkup);
 
+const cardsMarkup = createImgCards(galleryItems);  
+imgContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
 function createImgCards(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
@@ -25,16 +24,27 @@ function createImgCards(galleryItems) {
     `
     }).join('');
 }
+const imgItem = document.querySelector(".gallery__image");
+const imgLink = document.querySelector('.gallery__link');
+
+
 
 imgContainer.addEventListener('click', onContainerClick);
 
-function onContainerClick(event) {
-   
-  if (!event.target.classList.contains('gallery__link')) {
-     console.log('ссылка')
-   }
-  
-  
 
-  
+function onContainerClick(event) {
+
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }event.preventDefault();
+  event.target.src = event.target.dataset.source;
+  console.log(event.target.src);
+  console.log(event.target.dataset.source);
 }
+
+// imgLink.addEventListener('click',worker)
+
+// function worker(event) { 
+//  event.preventDefault();
+ 
+// } 
